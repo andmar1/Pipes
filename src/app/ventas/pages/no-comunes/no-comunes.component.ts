8726e62bb7cb77599008bd8ee8245ba148ec8ada
Antonio Andrade <antoniomaresmares001gmail.com>
2022-03-12 13:20:33 -0600

    i18 plural pipe, funcionamiento

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  nombre:string = "Antonio";
+  genero:string = "masculino";
 
-  ngOnInit(): void {
+  invitacionMapa ={
+    'masculino':'invitarlo',
+    'femenino':'invitarla'
   }
+
 }
